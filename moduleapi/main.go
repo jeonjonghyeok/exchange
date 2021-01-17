b5fd60f63dc32463f44821f246b3d0522f00739d
jeonjonghyeok <tongher1685@gmail.com>
2021-01-17 21:46:56 +0900

    module

---
+++ moduleapi/main.go
@@ -0,0 +1,15 @@
+package main // import "bitbucket.org/dream_yun/moduleapi"
+
+import (
+	"bitbucket.org/dream_yun/moduleapi/app"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.WithFields(log.Fields{
+		"Application": "TestApp",
+		"Version:":    "v0.1.1",
+	}).Info("Application start...")
+	myapp := app.New("test app")
+	myapp.Run()
+}
